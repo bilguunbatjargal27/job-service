16170e6adc2d5894a25c2bc8aafd68f0844513ff
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-09-20 21:27:02 -0500

    Job services update

---
+++ src/main/java/cs/mum/edu/orangeteam/compro/service/JobService.java
@@ -0,0 +1,13 @@
+package cs.mum.edu.orangeteam.compro.service;
+
+import cs.mum.edu.orangeteam.compro.model.Job;
+
+import java.util.List;
+
+public interface JobService {
+    public List<Job> getAllJobs();
+    public Job getJobById(Long id);
+    public Job addJob(Job job);
+
+
+}
