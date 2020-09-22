916df084cd7d607e8b1767504e14dc3be05cbd3f
Haftom-Mesfin <sadtime1906@gmail.com>
2020-09-22 09:44:06 -0500

    job search report model update

--- src/main/java/cs/mum/edu/orangeteam/compro/service/impl/JobServiceImpl.java
+++ src/main/java/cs/mum/edu/orangeteam/compro/service/impl/JobServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 @Transactional
 public class JobServiceImpl implements JobService {
